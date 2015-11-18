9e23261a7f9b494ba43fb14ad4bd9feed7508cb1
Daniel-MABD <daniel.mabd@gmail.com>
2015-11-18 22:30:14 +0800

    token白名单

--- src/main/java/com/rms/api/web/filter/AuthenticationFilter.java
+++ src/main/java/com/rms/api/web/filter/AuthenticationFilter.java
@@ -2,6 +2,9 @@ package com.rms.api.web.filter;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Properties;
 
 import javax.servlet.Filter;
@@ -36,8 +39,76 @@ public class AuthenticationFilter implements Filter{
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response,
 			FilterChain chain) throws IOException, ServletException {
-		response.setContentType("application/x-www-form-urlencoded; charset=utf-8");
-		chain.doFilter(request, response);
+
+		/*校验token*/
+		log.info("start authentic......");
+		HttpServletRequest httpRequest = (HttpServletRequest)request;
+		HttpServletResponse httpResonse = (HttpServletResponse)response;
+		httpResonse.setCharacterEncoding("utf-8");
+		String token = httpRequest.getHeader("token");
+		String url = httpRequest.getRequestURI();
+		
+		boolean authorized = true;
+
+		Resource resource = new ClassPathResource("/application.properties");
+		Properties props = PropertiesLoaderUtils.loadProperties(resource);
+		//取需要验证TOKEN的功能
+		String[] tokenApis =  props.getProperty("token.api").split(",");
+		HashSet tokenAPISet = new HashSet();		
+		tokenAPISet.addAll(Arrays.asList(tokenApis));
+		//判读是否需要验证TOKEN
+		boolean needToken = tokenAPISet.contains(httpRequest.getServletPath());
+		
+		if(needToken){
+			log.debug("checking token");
+			//验证TOKEN，通过返回手机号，否则为null
+			if(token== null){
+				PrintWriter writer = httpResonse.getWriter();
+				ResponseData data = new ResponseData(); 
+				data.setCode("400");
+				data.setMsg("no token");
+				writer.write(JsonUtil.object2Json(data));
+				try {
+					writer.flush();
+					writer.close();
+				} catch (Exception e) {
+					log.error("",e);
+				}
+				authorized = false;
+			}else{
+				String rms_url = props.getProperty("rms.url");
+				String phone = HttpClientUtil.doPost(rms_url, "system/checkToken", httpRequest);
+				log.debug("fetched phone by token:" + phone);
+				if(phone == null || phone.length()==0){
+					authorized = false;
+				}else{
+					authorized = true;
+					request.setAttribute("mobile", phone);	
+				}
+			}
+		}
+		
+		if(authorized){
+			response.setContentType("application/x-www-form-urlencoded; charset=utf-8");
+			chain.doFilter(request, response);
+		}else{
+			PrintWriter writer = httpResonse.getWriter();
+			ResponseData data = new ResponseData(); 
+			data.setCode("401");
+			data.setMsg("请登陆");
+			writer.write(JsonUtil.object2Json(data));
+			try {
+				writer.flush();
+				writer.close();
+			} catch (Exception e) {
+				log.error("",e);
+			}
+		}
+		
+		
+		
+		log.info("end authentic......");
+
 	}
 
 	@Override
@@ -45,11 +116,6 @@ public class AuthenticationFilter implements Filter{
 		// TODO Auto-generated method stub
 		
 	}
-	
-	//TODO
-	private boolean checkToken(String token){
-		//String res = HttpClientUtil.doPost("/checkToken", token);
-		return true;
-	}
+
 
 }
