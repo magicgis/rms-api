339b0a3bf5e85220391adc0777de5e72297cfbab
wsjerome <xiaopapasoftware@126.com>
2017-01-13 10:46:27 +0800

    优化

--- src/main/java/com/rms/api/web/entity/HeaderData.java
+++ src/main/java/com/rms/api/web/entity/HeaderData.java
@@ -3,13 +3,14 @@ package com.rms.api.web.entity;
 import java.io.Serializable;
 
 public class HeaderData implements Serializable {
-	private String token;
+  private static final long serialVersionUID = -6812113228366603615L;
+  private String token;
 
-	public String getToken() {
-		return token;
-	}
+  public String getToken() {
+    return token;
+  }
 
-	public void setToken(String token) {
-		this.token = token;
-	}
+  public void setToken(String token) {
+    this.token = token;
+  }
 }
--- src/main/java/com/rms/api/web/filter/AuthenticationFilter.java
+++ src/main/java/com/rms/api/web/filter/AuthenticationFilter.java
@@ -2,7 +2,6 @@ package com.rms.api.web.filter;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Properties;
@@ -21,102 +20,101 @@ import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PropertiesLoaderUtils;
-import org.springframework.util.StringUtils;
 
 import com.rms.api.web.entity.ResponseData;
 import com.rms.api.web.util.HttpClientUtil;
 import com.rms.api.web.util.JsonUtil;
 
-public class AuthenticationFilter implements Filter{
-	Logger log = LoggerFactory.getLogger(AuthenticationFilter.class);
-	
-	@Override
-	public void destroy() {
-		// TODO Auto-generated method stub
-		
-	}
+public class AuthenticationFilter implements Filter {
+  Logger log = LoggerFactory.getLogger(AuthenticationFilter.class);
 
-	@Override
-	public void doFilter(ServletRequest request, ServletResponse response,
-			FilterChain chain) throws IOException, ServletException {
+  @Override
+  public void destroy() {
+    // TODO Auto-generated method stub
 
-		/*校验token*/
-		log.info("start authentic......");
-		HttpServletRequest httpRequest = (HttpServletRequest)request;
-		HttpServletResponse httpResonse = (HttpServletResponse)response;
-		log.info("request path: " + httpRequest.getServletPath());
-		//httpResonse.setCharacterEncoding("utf-8");
-		String token = httpRequest.getHeader("token");
-		String url = httpRequest.getRequestURI();
-		
-		boolean authorized = true;
+  }
 
-		Resource resource = new ClassPathResource("/application.properties");
-		Properties props = PropertiesLoaderUtils.loadProperties(resource);
-		//取需要验证TOKEN的功能
-		String[] tokenApis =  props.getProperty("token.api").split(",");
-		HashSet tokenAPISet = new HashSet();		
-		tokenAPISet.addAll(Arrays.asList(tokenApis));
-		//判读是否需要验证TOKEN
-		boolean needToken = tokenAPISet.contains(httpRequest.getServletPath());
-		
-		if(needToken){
-			log.debug("checking token");
-			//验证TOKEN，通过返回手机号，否则为null
-			if(token== null){
-				PrintWriter writer = httpResonse.getWriter();
-				ResponseData data = new ResponseData(); 
-				data.setCode("400");
-				data.setMsg("no token");
-				writer.write(JsonUtil.object2Json(data));
-				try {
-					writer.flush();
-					writer.close();
-				} catch (Exception e) {
-					log.error("",e);
-				}
-				authorized = false;
-			}else{
-				String rms_url = props.getProperty("rms.url");
-				String phone = HttpClientUtil.getData(rms_url, "system/checkToken", httpRequest);
-				log.debug("fetched phone by token:" + phone);
-				if(phone == null || phone.length()==0){
-					authorized = false;
-				}else{
-					authorized = true;
-					request.setAttribute("mobile", phone);	
-				}
-			}
-		}
-		
-		if(authorized){
-			//response.hsetContentType("application/x-www-form-urlencoded; charset=utf-8");
-			chain.doFilter(request, response);
-		}else{
-			PrintWriter writer = httpResonse.getWriter();
-			ResponseData data = new ResponseData(); 
-			data.setCode("401");
-			data.setMsg("请登陆");
-			writer.write(JsonUtil.object2Json(data));
-			try {
-				writer.flush();
-				writer.close();
-			} catch (Exception e) {
-				log.error("",e);
-			}
-		}
-		
-		
-		
-		log.info("end authentic......");
+  @SuppressWarnings({"unused", "unchecked", "rawtypes"})
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
-	}
+    /* 校验token */
+    log.info("start authentic......");
+    HttpServletRequest httpRequest = (HttpServletRequest) request;
+    HttpServletResponse httpResonse = (HttpServletResponse) response;
+    log.info("request path: " + httpRequest.getServletPath());
+    // httpResonse.setCharacterEncoding("utf-8");
+    String token = httpRequest.getHeader("token");
+    String url = httpRequest.getRequestURI();
 
-	@Override
-	public void init(FilterConfig arg0) throws ServletException {
-		// TODO Auto-generated method stub
-		
-	}
+    boolean authorized = true;
+
+    Resource resource = new ClassPathResource("/application.properties");
+    Properties props = PropertiesLoaderUtils.loadProperties(resource);
+    // 取需要验证TOKEN的功能
+    String[] tokenApis = props.getProperty("token.api").split(",");
+    HashSet tokenAPISet = new HashSet();
+    tokenAPISet.addAll(Arrays.asList(tokenApis));
+    // 判读是否需要验证TOKEN
+    boolean needToken = tokenAPISet.contains(httpRequest.getServletPath());
+
+    if (needToken) {
+      log.debug("checking token");
+      // 验证TOKEN，通过返回手机号，否则为null
+      if (token == null) {
+        PrintWriter writer = httpResonse.getWriter();
+        ResponseData data = new ResponseData();
+        data.setCode("400");
+        data.setMsg("no token");
+        writer.write(JsonUtil.object2Json(data));
+        try {
+          writer.flush();
+          writer.close();
+        } catch (Exception e) {
+          log.error("", e);
+        }
+        authorized = false;
+      } else {
+        String rms_url = props.getProperty("rms.url");
+        String phone = HttpClientUtil.getData(rms_url, "system/checkToken", httpRequest);
+        log.debug("fetched phone by token:" + phone);
+        if (phone == null || phone.length() == 0) {
+          authorized = false;
+        } else {
+          authorized = true;
+          request.setAttribute("mobile", phone);
+        }
+      }
+    }
+
+    if (authorized) {
+      // response.hsetContentType("application/x-www-form-urlencoded; charset=utf-8");
+      chain.doFilter(request, response);
+    } else {
+      PrintWriter writer = httpResonse.getWriter();
+      ResponseData data = new ResponseData();
+      data.setCode("401");
+      data.setMsg("请登陆");
+      writer.write(JsonUtil.object2Json(data));
+      try {
+        writer.flush();
+        writer.close();
+      } catch (Exception e) {
+        log.error("", e);
+      }
+    }
+
+
+
+    log.info("end authentic......");
+
+  }
+
+  @Override
+  public void init(FilterConfig arg0) throws ServletException {
+    // TODO Auto-generated method stub
+
+  }
 
 
 }
